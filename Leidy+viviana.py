7045945e49872a8ee9f20c72a8a0f8572ec63720
ladyvivi-17 <ledy19vivi@gmail.com>
2016-08-08 20:40:43 -0500

    Add files via upload

---
+++ Leidy+viviana.py
@@ -0,0 +1,154 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print "bebe"
+
+$-\sum_{p}\displaystyle\lim_{}{}(x^p)-\log{}(2)$
+# $-\sum_{p}\displaystyle\lim_{}{}(x^p)-\log{}(2)$
+# 
+
+# In[2]:
+
+2+4*6/8.0-3
+
+
+# In[3]:
+
+3/2
+
+
+# In[4]:
+
+1/0
+
+
+# #vivi
+# 
+# 
+
+# In[5]:
+
+3+5j
+
+
+# In[6]:
+
+(3+5j)-(1-1j)
+
+
+# In[7]:
+
+5**3
+
+
+# In[8]:
+
+5**3
+
+
+# In[9]:
+
+5 ** 4
+
+
+
+# In[10]:
+
+int(_16)
+
+
+# In[11]:
+
+int(_2)
+
+
+# In[12]:
+
+5+9.0
+
+
+# In[13]:
+
+5+9.5
+
+
+# In[14]:
+
+round(_13)
+
+
+# In[15]:
+
+max(9,30,12,1,15)
+
+
+# In[16]:
+
+num=10
+
+
+# In[17]:
+
+num
+
+
+# In[18]:
+
+a,b,c,d,e=1,2,3,4,5
+
+
+# In[19]:
+
+a
+
+
+# In[20]:
+
+c
+
+
+# In[21]:
+
+d
+
+
+# In[22]:
+
+if a>e:
+    print ("El mayor es" . e)
+    else print ("el menor es" . a)
+
+
+# In[23]:
+
+if a>e:
+    print ("El mayor es" , e)
+    else print ("el menor es" , a)
+
+
+# In[24]:
+
+if a>e:
+    print ("El mayor es" , e)
+    else: print ("el menor es" , a)
+
+
+# In[28]:
+
+if a > e :
+        print "El mayor es" , e
+else: 
+        print "el menor es" , a
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
